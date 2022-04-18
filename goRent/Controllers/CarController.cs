522185522deb5ec985f3a8b682e6e31e5fa17736
Amor_gh13@student.itstep.bg <Amor_gh13@student.itstep.bg>
2022-04-18 18:36:37 +0300

    Prepare for datebase

--- goRent/Controllers/CarController.cs
+++ goRent/Controllers/CarController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using goRent.Class;
+using goRent.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace goRent.Controllers
 {
     public class CarController : Controller
     {
+        private readonly ICarService rentCarService;
+
+        public CarController(ICarService rentCarService)
+        {
+            this.rentCarService = rentCarService;
+        }
+
         public IActionResult Index()
         {
+            return View(this.rentCarService.GetCar());
+        }
+
+        [HttpGet]
+        public IActionResult AddCar()
+        {
+
             return View();
+        }
+
+        public IActionResult Cities()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveCar(Car car)
+        {
+            this.rentCarService.AddCar(car);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetCar(int id)
+        {
+            var car = this.rentCarService.GetById(id);
+            return View(car);
+        }
+
+        public IActionResult EditCar(Car carToEdit)
+        {
+            this.rentCarService.EditCar(carToEdit);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCar(int id)
+        {
+            this.rentCarService.DeleteCar(id);
+            return RedirectToAction("Index");
         }
     }
 }
